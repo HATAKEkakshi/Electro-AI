a95bc265b666678a3ac0e9c1ab59cba8ad4db597
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-09-14 20:51:59 +0530

    Model

--- model/model(pickle)/lstm.py
+++ model/model(pickle)/lstm.py
@@ -1,4 +1,100 @@
-import streamlit as st
+import numpy as np
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
 
-st.title("Electro-AI Model Backend")
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import r2_score
 
+import tensorflow as tf
+from keras.layers import Dense,Dropout,SimpleRNN,LSTM
+from keras.models import Sequential
+
+import warnings
+warnings.filterwarnings("ignore")
+
+#dataset loading 
+df = pd.read_csv("/Users/hemantkumar/Developer/hackathon/sih/model/dataset/DOM_hourly.csv")
+# convert the Datetime column to Datetime format
+df['Datetime'] = pd.to_datetime(df['Datetime'])
+
+# indexing the Datetime column after the transformation
+df.set_index('Datetime', inplace=True)
+# Let see at the years in the data set
+years = df.index.year.unique()
+# Let's see the average energy consumed per year
+df_yearly_avg = df['DOM_MW'].resample('Y').mean()
+#Normalizing Dataset
+def normalize_data(df):
+    scaler = MinMaxScaler()
+    normalized_data = scaler.fit_transform(df['DOM_MW'].values.reshape(-1,1))
+    df['DOM_MW'] = normalized_data
+    return df, scaler
+
+df_norm, scaler = normalize_data(df)
+df_norm.shape
+# 2017-02-13 after this date we will choose the test set
+split_date = '2017-02-13'
+
+DOM_train = df_norm.loc[df_norm.index <= split_date].copy()
+DOM_test = df_norm.loc[df_norm.index > split_date].copy()
+
+def load_data(data, seq_len):
+    X_train = []
+    y_train = []
+
+    for i in range(seq_len, len(data)):
+        X_train.append(data.iloc[i-seq_len : i, 0])
+        y_train.append(data.iloc[i, 0])
+
+    # last 6189 days are going to be used in test
+    X_test = X_train[110000:]
+    y_test = y_train[110000:]
+
+    # first 110000 days are going to be used in training
+    X_train = X_train[:110000]
+    y_train = y_train[:110000]
+
+    # convert to numpy array
+    X_train = np.array(X_train)
+    y_train = np.array(y_train)
+
+    X_test = np.array(X_test)
+    y_test = np.array(y_test)
+
+    # reshape data to input into RNN&LSTM models
+    X_train = np.reshape(X_train, (110000, seq_len, 1))
+
+    X_test = np.reshape(X_test, (X_test.shape[0], seq_len, 1))
+
+    return [X_train, y_train, X_test, y_test]
+seq_len = 20
+
+# Let's create train, test data
+X_train, y_train, X_test, y_test = load_data(df, seq_len)
+
+print('X_train.shape = ',X_train.shape)
+print('y_train.shape = ', y_train.shape)
+print('X_test.shape = ', X_test.shape)
+print('y_test.shape = ',y_test.shape)
+
+#accesing model
+with open('/Users/hemantkumar/Developer/hackathon/sih/model/model(pickle)/rnnmodel.pkl','rb') as f :
+    rnn=pickle.load(f)
+rnn_predictions=rnn.predict(X_test)
+rnn_score = r2_score(y_test,rnn_predictions)
+print("R2 Score of RNN model = ",rnn_score)
+# Reverse transform scaler to convert to real values
+y_test_inverse = scaler.inverse_transform(y_test.reshape(-1, 1))
+rnn_predictions_inverse = scaler.inverse_transform(rnn_predictions)
+
+# Get values after inverse transformation
+y_test_inverse = y_test_inverse.flatten()
+rnn_predictions_inverse = rnn_predictions_inverse.flatten()
+last_6169_index_dates = df.index[-6169:]
+
+# Now let's see our actual y and predicted y values as dataframes
+results_RNN = pd.DataFrame({"Date":last_6169_index_dates, 'Actual': y_test_inverse, 'Predicted': rnn_predictions_inverse})
+print(results_RNN)
+
+print(rnn_predictions_inverse)
